40b6fbe3b6a42a9bc4b0473cbe51daa787ebaa1e
Richard Gaunt <richard.gaunt@protonmail.com>
2024-05-02 09:52:51 +1000

    Updated relative date trait to use current second.

--- src/DateTrait.php
+++ src/DateTrait.php
@@ -87,7 +87,7 @@ trait DateTrait {
 
     // If `now` is not provided, round to the current hour to make sure that
     // assertions are running within the same timeframe (for long tests).
-    $now = $now ?: strtotime(date('Y-m-d H:i:00', time()));
+    $now = $now ?: strtotime(date('Y-m-d H:i:s', time()));
 
     return preg_replace_callback('/\[([relative:]+):([^]\[#]+)(?:#([^]\[]+))?]/', function (array $matches) use ($now) {
       $timestamp = strtotime($matches[2], $now);
